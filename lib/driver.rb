abf1693b4bac915b9c926939cdec306c44fa4d3b
ishibashi.futos <ishibashi.futos@jp.fujitsu.com>
2018-12-02 18:49:06 +0900

    add new file

---
+++ lib/driver.rb
@@ -0,0 +1,42 @@
+require 'bundler/setup'
+require 'selenium-webdriver'
+require 'date'
+
+class WebDriver
+
+  def initialize()
+    @driver = Selenium::WebDriver.for :chrome
+  end
+
+  def getReservationData()
+    # apahotelの予約ページを開く
+    @driver.navigate.to 'https://www.apahotel.com/hotel/shutoken/tokyo_makuhari_bay/'
+    # calendar detailのデータを取得
+    calendar = @driver.find_elements(:class, 'cl-detail')
+    # nの値が"-"以外 ≒ 当月以外を取得
+    calendar = calendar.map do |n|
+      if n.text != '-'
+        n.text
+      end
+    end
+    # 取得が完了したらwebdriverを終了する
+    dispose()
+    # nilを消す
+    calendar.compact!
+
+    # 戻り値となるハッシュを用意
+    calendarData = Hash.new
+
+    calendar.each_with_index do |n, i|
+      # データを保存・・・なぜか2日からスタートなので+2。普段は+1でいいはず
+      calendarData.store(Date.new(2018, 12, i + 2).to_s, n)
+    end
+    calendarData
+  end
+
+  def dispose()
+    @driver.quit
+    @driver = nil
+  end
+
+end
---
+++ lib/main.rb
@@ -0,0 +1,5 @@
+require_relative './driver'
+
+driver = WebDriver.new
+
+p driver.getReservationData()
