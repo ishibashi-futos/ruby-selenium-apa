44cdb80ab42a40ae9e559c1b877614c55cd1497e
ishibashi.futos <ishibashi.futos@jp.fujitsu.com>
2018-12-02 22:29:20 +0900

    改行位置の調整

--- lib/main.rb
+++ lib/main.rb
@@ -6,7 +6,7 @@ driver = WebDriver.new
 
 message_hash_data = driver.get_reservation_data
 
-messages = "Execute Date [#{Time.now}\n]"
+messages = "Execute Date [#{Time.now}]\n"
 
 ['2018-12-29', '2018-12-30', '2018-12-31'].each do |date|
   messages += "#{date} : #{message_hash_data.fetch(date)}\n"
